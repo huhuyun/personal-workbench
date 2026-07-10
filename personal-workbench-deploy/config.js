6a0cd6155834e933bf8e905631d7550e2efffdc0
huhuyun <13869385497@163.com>
2026-07-10 14:03:30 +0800

    Delete personal-workbench-deploy directory

--- personal-workbench-deploy/config.js
+++
@@ -1,5 +0,0 @@
-window.WORKBENCH_CONFIG = {
-  SUPABASE_URL: "https://djolalodahjoynknglng.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqb2xhbG9kYWhqb3lua25nbG5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0OTcxNDcsImV4cCI6MjA5OTA3MzE0N30.QRben_4_8Gj7MMP_ulbXhJP6kEWi3hZmweZkVUYL55I",
-  CLOUD_TABLE: "workbench_states"
-};
